7fdc79dfaf4af57d8b103a33d2a499ccf44281a0
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-16 10:15:06 -0500

    next step fix with thymleaf

--- src/main/java/com/codeup/springboot_blog/controllers/HomeController.java
+++ src/main/java/com/codeup/springboot_blog/controllers/HomeController.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//@Controller
+//public class HomeController {
+//    @GetMapping("/")
+//    public String hello() {
+//        return "home";
+//    }
+//}
+
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String hello() {
         return "home";
     }
+
+    @GetMapping("/google")
+    public String google() {return "googleBookIDBuild";}
 }
