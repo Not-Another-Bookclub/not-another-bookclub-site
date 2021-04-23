6dd1bdbfc03c7c1ae4a19c584186a59918c28986
Walter Smith <walter.c.smith01@gmail.com>
2021-04-23 12:35:39 -0400

    Feat: Added an alert to notify the user that the email was sent

--- src/main/java/com/codeup/springboot_blog/controllers/UserController.java
+++ src/main/java/com/codeup/springboot_blog/controllers/UserController.java
@@ -93,6 +93,7 @@ public class UserController {
             emailService.prepareAndSend(email,user1.getUsername(),user1.getUsername());
 
             model.addAttribute("email", user1.getUsername());
+
             model.addAttribute("alert", "<div class=\"alert alert-success\" role=\"alert\">\n" +
                     "  The email was successfully sent. </div>");
 
